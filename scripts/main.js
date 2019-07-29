35e923587de21e7660d41fd0d2913e9a53676d77
martagrio <marta.igr90@gmail.com>
2019-07-29 09:34:08 +0200

    Add listener and handler for the button

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,8 @@ const insert = document.querySelector('.number');
 const button = document.querySelector('.btn');
 const tip = document.querySelector('.tips');
 
+console.log(tip.innerHTML);
+
 // Función que genera un número aleatorio hasta un máximo dado
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
@@ -14,3 +16,17 @@ function getRandomNumber(max) {
 const myRandomNumber = getRandomNumber(100);
 
 console.log(`Mi número aleatorio es ${myRandomNumber}`);
+
+function showNumber() {
+    const myNumber = parseInt(insert.value);
+
+    if (myNumber === myRandomNumber) {
+        tip.innerHTML = '¡HAS GANADO, CAMPEONA!';
+    } else if (myNumber < myRandomNumber) {
+        tip.innerHTML = "demasiado bajo";
+    } else {
+        tip.innerHTML = "demasiado alto";
+    }
+}
+
+button.addEventListener('click', showNumber);
