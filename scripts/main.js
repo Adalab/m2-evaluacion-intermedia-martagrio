530a1edb8abfe0853fcba2af9028f1a687f90ed4
martagrio <marta.igr90@gmail.com>
2019-07-29 09:21:30 +0200

    Add html and js basic content

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const counter = document.querySelector('.counter');
+const insert = document.querySelector('.number');
+const button = document.querySelector('.btn');
+const tip = document.querySelector('.tips');
+
+console.log(tip);
