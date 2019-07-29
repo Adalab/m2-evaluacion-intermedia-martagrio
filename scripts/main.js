3e56ad312b598ff473c27ea8f898d39667caee99
martagrio <marta.igr90@gmail.com>
2019-07-29 09:49:46 +0200

    Add styles

--- scripts/main.js
+++ scripts/main.js
@@ -12,7 +12,6 @@ function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
 
-// Anda, chacho, píntame en la consola un número random hasta 100;
 const myRandomNumber = getRandomNumber(100);
 
 console.log(`Mi número aleatorio es ${myRandomNumber}`);
