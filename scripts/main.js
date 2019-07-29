9683a5490ad571a5f1f57c353461e6bd581a47e1
martagrio <marta.igr90@gmail.com>
2019-07-29 09:24:25 +0200

    Add random function

--- scripts/main.js
+++ scripts/main.js
@@ -5,4 +5,12 @@ const insert = document.querySelector('.number');
 const button = document.querySelector('.btn');
 const tip = document.querySelector('.tips');
 
-console.log(tip);
+// Función que genera un número aleatorio hasta un máximo dado
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+// Anda, chacho, píntame en la consola un número random hasta 100;
+const myRandomNumber = getRandomNumber(100);
+
+console.log(`Mi número aleatorio es ${myRandomNumber}`);
