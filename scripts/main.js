58ad1f1259c009cd278627af9d2634a9bb279d8d
martagrio <marta.igr90@gmail.com>
2019-07-31 23:55:57 +0200

    Add label tag and id for input and counter logic

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,8 @@ const insert = document.querySelector('.number');
 const button = document.querySelector('.btn');
 const tip = document.querySelector('.tips');
 
-console.log(tip.innerHTML);
+let clicks = 0;
 
-// Función que genera un número aleatorio hasta un máximo dado
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
@@ -18,16 +17,35 @@ console.log(`Mi número aleatorio es ${myRandomNumber}`);
 
 function showNumber() {
     const myNumber = parseInt(insert.value);
+    console.log(myNumber);
     const myCounter = counter.innerHTML;
-    counter.innerHTML = parseInt(myCounter) + 1;
 
     if (myNumber === myRandomNumber) {
         tip.innerHTML = '¡HAS GANADO, CAMPEONA!';
+
+    } else if (myNumber !== Number) {
+        tip.innerHTML = "No has introducido ningún número, por favor, introduce un número del 0 al 100";
+
+    } else if (myNumber > 100 || myNumber < 0) {
+        tip.innerHTML = "Por favor, introduce un número del 0 al 100";
+
     } else if (myNumber < myRandomNumber) {
         tip.innerHTML = "demasiado bajo";
-    } else {
+        clicks++;
+    } else if (myNumber > myRandomNumber) {
         tip.innerHTML = "demasiado alto";
+        clicks++;
     }
+    counter.innerHTML = clicks;
 }
 
 button.addEventListener('click', showNumber);
+
+
+function preshEnter(event) {
+    if (event.keyCode === 13) {
+        showNumber();
+    }
+}
+
+window.addEventListener('keydown', preshEnter);
